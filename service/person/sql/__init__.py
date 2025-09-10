bc7609a1400b383f5204105f8b7d2be91d33ab8b
Lens-Chaédarson Philismé <73264885+lensphilisme@users.noreply.github.com>
2025-09-09 21:38:02 -0400

    Update __init__.py

--- service/person/sql/__init__.py
+++ service/person/sql/__init__.py
@@ -1,6 +1,6 @@
 import constants
 from commonsql import Q_IS_ALLOWED_CLUB_NAME, Q_COMPUTED_FLAIR
-from service.api.utils import compute_personality_vectors, answer_score_vectors
+from service.api.utils import compute_personality_vectors
 
 
 MAX_CLUB_SEARCH_RESULTS = 20
